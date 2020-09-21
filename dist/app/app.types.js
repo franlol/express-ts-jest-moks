66da0abe2dd65c11320242d941f19c5aa8d21a18
franlol <fraaaaan@gmail.com>
2020-09-22 00:48:55 +0200

    :see_no_evil: Adding ./dist to gitignore

--- dist/app/app.types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APP = void 0;
-var APP;
-(function (APP) {
-    APP["ON_LIVE"] = "Server running on port:";
-})(APP = exports.APP || (exports.APP = {}));
